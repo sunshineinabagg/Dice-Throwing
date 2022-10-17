b1f6c0a222dae6f6568868cddfb519de002878a1
sunshineinabagg <sunshineinabagg@yandex.ru>
2022-10-18 00:36:40 +0300

    Separate to module.py
    Add module.py

--- main.py
+++ main.py
@@ -1,14 +1,4 @@
-import random
-
-class DiceThrowing:
-    def __init__(self, edges):
-        self.edges = edges
-
-    def throw(self, amount):
-        result = []
-        for i in range(amount):
-            result.append(random.randint(1, self.edges))
-        return result   
+from module import DiceThrowing
 
 dice = DiceThrowing(20)
 print(dice.throw(5))
---
+++ module.py
@@ -0,0 +1,11 @@
+import random
+
+class DiceThrowing:
+    def __init__(self, edges):
+        self.edges = edges
+
+    def throw(self, amount):
+        result = []
+        for i in range(amount):
+            result.append(random.randint(1, self.edges))
+        return result   
